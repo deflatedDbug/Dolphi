6f3e356a6dba752e7323a4c86d57a77c57cb3f60
emilHof <95590295+emilHof@users.noreply.github.com>
2022-09-17 15:53:18 -0400

    flused out login page

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+import Image from 'next/image'
 import { Button } from 'react-bootstrap';
 import { Dispatch, SetStateAction } from "react";
+import authenticateEmail from './api/auth'
+import logo from "../content/dolphi_logo.png"
 
 // hexes: #142029, #3F7396, #4F8EB5, #77BDDF, #A4E2F9
 // TODO Login Header
@@ -17,29 +22,77 @@ interface LoginProps {
 function Logo() {
 	return(
 		<div 
-		style={{background: "#77BDDF", width: "40vw", 
-				height: "8vh", margin: "auto", display: "flex",
+		style={{background: "#77BDDF", width: "100%",
+				height: "5rem", margin: "auto", display: "flex",
 				alignContent: "center", justifyContent: "center",
-				borderRadius: ".5rem"}}
+				borderRadius: ".5rem", marginBottom: "1rem",
+				boxShadow: "0px 0px 5px rgba(0, 0, 0, .3)"}}
 		>
-			<h3 style={{margin: "auto", fontWeight: "bolder", fontSize: "3rem"}}> Dophi </h3>
+			<div style={{margin: "auto", display: "grid", 
+				gridTemplateColumns: "1fr 2fr 1fr", columnGap: "1rem",
+				height: "4rem"}}>
+				<Image src={logo} alt={"Logo"} layout={"responsive"} 
+				/>
+				<h3 style={{margin: "auto", 
+				fontWeight: "bolder", fontSize: "3rem"}}> 
+					Dolphi 
+				</h3>
+			</div>
 		</div>
 	)
 }
 
-function LoginPage({loggedInFunc}: LoginProps): JSX.Element {
+function validateForm(email: string): boolean {
+	return email.split(".").find(element => element === "edu") != undefined
+}
+
+function EmailForm({loggedInFunc}: LoginProps): JSX.Element {
+	const [email, setEmail] = useState("");
+
 	return(
-		<>
-			<div style={{display: "flex", flexDirection: "column",justifyContent: "center", alignContent: "center", margin: "auto"}}>
+		<Form>
+		  <Form.Group className="mb-3" controlId="formBasicEmail">
+			<Form.Label style={{fontSize: ".75rem"}}>Email Address</Form.Label>
+			<Form.Control type="email" 
+			onChange={e => setEmail(e.target.value)}
+			placeholder="Enter email" />
+			<Form.Text className="text-muted">
+				Must be a .edu address.
+			</Form.Text>
+		  </Form.Group>
+
+		  <Button variant="primary" type="submit" 
+				style={{background: "#4F8EB5", width: "100%"}}
+				onClick={(e)=> {
+					e.preventDefault()
+					if (validateForm(email)) loggedInFunc(authenticateEmail())
+				}}>
+				Submit
+		  </Button>
+		</Form>
+	)
+}
+
+function LoginPage({loggedInFunc}: LoginProps): JSX.Element {
+
+	return(
+		<div style={{background: "rgba(240, 240, 240, 1)", height: "100vh",
+			display: "flex", justifyContent: "center"}}>
+			<div style={{display: "flex", flexDirection: "column",
+				justifyContent: "center", alignContent: "center", 
+				margin: "auto", width: "40vw",
+				}}>
 				<Logo/>
-				<Button 
-					style={{background: "#4F8EB5"}}
-					onClick={()=>loggedInFunc(true)}
-					>
-					hello there! 
-				</Button>
+				<div style={{background: "white", display: "flex",
+					justifyContent: "center", alignContent: "center",
+					padding: "1rem", flexDirection: "column",
+					boxShadow: "0px 0px 10px rgba(0, 0, 0, .1)"
+					}}>
+					<h3 style={{margin: "auto"}}> Welcome </h3>
+					<EmailForm loggedInFunc={loggedInFunc}/>
+				</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
